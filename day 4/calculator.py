52e3b141dfe73e4d99b9b728a58e73e0ed26e449
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2023-02-06 13:22:30 +0530

    Add files via upload
    functions,whileloop

---
+++ day 4/calculator.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 14:30:28 2021
+
+@author: Prasath
+"""
+
+def  add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
---
+++ day 4/func.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 14:33:06 2021
+
+@author: Prasath
+"""
+
+from calculator import add
+
+a = 4
+b = 6
+
+y = add(a,b)
+d = sub(a,b)
+#e = add.mul(a,b)
