e2bd63c1acb27d2fbe6b645c1b2bf7eb8d9caa05
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2023-02-07 20:57:27 +0530

    Add files via upload
    classes and object ,exception handling

---
+++ day 5/exception.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 11:33:57 2021
+
+@author: Prasath
+"""
+
+try:
+    num1 = int(input("Enter the number 1:"))
+    num2 = int(input("Enter the number 2:"))
+    num3 = num1/num2
+    print(num3)
+    
+    
+    List = [1,2,3]
+    print("List Value:", List[3])
+    
+    print(num)
+    
+except ZeroDivisionError:
+    print("one is not divisible by zero")
+
+except NameError:
+    print("Name is not defined")
+    
+except IndexError:
+    print("Index not found")
+
+        
+    
