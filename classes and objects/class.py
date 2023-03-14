f8cb4f70f5eb409ba62609cd9e91e5305fdfe91f
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2023-03-14 14:23:26 +0530

    Add files via upload
    python classes and objects

---
+++ classes and objects/class.py
@@ -0,0 +1,5 @@
+class mine():
+    x=2
+a=mine()
+print(a.x) # output is 2
+print("data type is ",mine())
---
+++ classes and objects/init.py
@@ -0,0 +1,13 @@
+class person:
+
+    def __init__(s,name,age):
+        s.name=name
+        s.age=age
+
+p=person("ak",100)
+p.name="AK"
+p.age=90
+print("with object",p.name,p.age)
+
+
+
