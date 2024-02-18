a925d23fb463c8683f6d4548f6b802a20a764e26
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2024-02-18 21:30:38 +0530

    Delete DSA directory

--- DSA/team.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-count=0
-for i in range(n):
-    a=input()
-    if a.count('2')>=1:
-        count+=1
-print(count)
-        
--- DSA/watermelon.py
+++
@@ -1,6 +0,0 @@
-a=int(input())
-b=a//2
-if b%2==0:
-    print("YES")
-else:
-    print("NO")
