4f9ae6268177015b3dbd2d034653f8023da82e73
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2024-02-22 22:24:02 +0530

    Delete sorting algorithms directory

--- sorting algorithms/Bubble_sort.py
+++
@@ -1,8 +0,0 @@
-arr=[2,1,45,12]
-n=len(arr)  # n=4
-for i in range(n):  # i is the index of elements.
-    for j in range(0,n-1):
-        if arr[j]>arr[j+1]:
-            arr[j],arr[j+1]=arr[j+1],arr[j]
-for i in range(n):
-    print(arr[i])
